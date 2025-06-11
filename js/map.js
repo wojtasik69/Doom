e4e676862959ae1d7b42246c3b9e3d8cb07067f4
wojtasik69 <kubuswojtasik@gmail.com>
2025-06-11 10:05:52 +0200

    Create map.js

---
+++ js/map.js
@@ -0,0 +1,17 @@
+// Definicja mapy (1 = ściana, 0 = puste)
+const MAP = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+const TILE_SIZE = 64; // Rozmiar jednej kratki na mapie w pikselach
+const MAP_WIDTH = MAP[0].length;
+const MAP_HEIGHT = MAP.length;
